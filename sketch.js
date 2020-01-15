385aa582171f0a122b22d079f0cfea6112774106
MarvynBailly <marvyn@marvyn.com>
2020-01-14 18:36:56 -0800

    Messing around with fractals

--- sketch.js
+++ sketch.js
@@ -1,10 +1,30 @@
+var length;
+var direction;
+var per_length = 0;
+var per_direction = 0;
+
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0,0);
   canvas.style('z-index','-1');
-  background();
-}
-function draw() {
+  background(255,255,255);
+
+  stroke(255,0,0);
+  translate(windowWidth/2,windowHeight);
+  translate(0, -height+448);
+  
+  drawTree(100,0)
+}  
+
+function drawTree(length,direction) {
+  if (length > 1){
+    per_length = per_length + length;
+    per_direction = per_direction + direction;
+    print(per_length)
+    line(0,per_length,0,per_length + length);
+    //drawtrunk(-length,direction+20);
+    //drawtrunk(-length/2,direction-20);
+    drawTree(length/2,direction);
+  }
 
 }
-
