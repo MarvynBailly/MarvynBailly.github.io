9ee32cc7b559a1fc0d8dd7230210acfab2b001a9
MarvynBailly <marvyn@marvyn.com>
2020-05-18 13:10:52 -0700

    Small noise

--- personal_projects/noise/noise2/sketch.js
+++ personal_projects/noise/noise2/sketch.js
@@ -3,14 +3,12 @@ var points = [];
 function setup(){
   createCanvas(400,400);
   pixelDensity(1);
-  
-  
-  
+
   for(let i = 0; i < 50; i++){
-    let p = p5.Vector.random3D();
+    let p = p5.Vector.random2D();
     let v = p.mult(random(200));
-    let m = v.add(200,200,0);
-    points[i] = m;
+    let m = v.add(200,200);
+    points[i] = m;//createVector(random(width),random(height),random(width));
   }
 }
 
@@ -24,15 +22,16 @@ function draw(){
       
       let distances = [];
       for (let i = 0; i < points.length; i++){
-        let z = frameCount % width;
-        distances[i] = dist(x,y,z,points[i].x,points[i].y,points[i].z);
+        //let z = frameCount % width;
+        let a = points[i];
+        distances[i] = dist(x,y,a.x,a.y);
       }
       
-      let n = 0;
+      //let n = 0;
       let sorted = sort(distances);
-      let r = map(sorted[n],0,150,0,255);
-      let g = map(sorted[n],0,150,0,255);
-      let b = map(sorted[n],0,150,0,255);
+      let r = map(sorted[0],0,150,0,255);
+      let g = map(sorted[1],0,150,0,255);
+      let b = map(sorted[2],0,150,0,255);
       let index = (x + y * width) * 4;
 
       pixels[index] = r;
