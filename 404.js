8a21d439a78cf2e598b74374b5a28e7ba1d463e9
MarvynBailly <marvyn@marvyn.com>
2020-06-07 15:28:43 -0700

    404 test

---
+++ 404.js
@@ -0,0 +1,15 @@
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  textSize(100);
+
+}  
+
+
+
+function draw(){
+  background(0);
+  fill(255);
+  stroke(255);
+  text('4 0 4',200,200);
+
+}
